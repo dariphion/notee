d9427ca83732d24e6f8eb6bb046cab6961ca7d1e
Mykyta <nikitafursov174@gmail.com>
2024-09-05 23:05:33 +0200

    Initial commit: Project setup

--- src/main/java/com/dariphion/notee/NoteeApplication.java
+++ src/main/java/com/dariphion/notee/NoteeApplication.java
@@ -2,7 +2,6 @@ package com.dariphion.notee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class NoteeApplication {
